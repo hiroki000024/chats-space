c2bbb9f127e48a324132955cc7ac310ad58c55a0
hiroki000024 <king0000240000@gmail.com>
2019-10-14 17:40:55 +0900

    message.js api conplete

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,92 @@
-$(function() {
-  $('.new_message').on('submit', function(){
-    console.log('hoge');
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+  $('.new_message').on('submit', function(e){
+   e.preventDefault();
+　　var formData = new FormData(this);
+   var url = $(this).attr('action')
+     $.ajax({
+       url: url,
+       type: "POST",
+       data: formData,
+       dataType: 'json',
+       processData: false,
+       contentType: false
+     })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.messages').append(html);
+       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+       $('form')[0].reset();
+     })
+      .fail(function(){
+        alert('error');
+      });
+      return false;
+    });
   });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function(){
+//   $('#new_message').on('submit', function(e) {
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action')
+//     $.ajax({
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     });
+//   });
+// })
