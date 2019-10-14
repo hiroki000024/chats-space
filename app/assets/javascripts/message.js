8d414dca9b780620453998fc6f876a85d4fe43d3
hiroki000024 <king0000240000@gmail.com>
2019-10-14 17:50:39 +0900

    コメントアウト　削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -74,19 +74,3 @@ $(function(){
 
 
 
-
-// $(function(){
-//   $('#new_message').on('submit', function(e) {
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action')
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     });
-//   });
-// })
