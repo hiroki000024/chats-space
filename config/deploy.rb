60b96b54fe775eaf69d183a64131bd1097373e08
hiroki000024 <king0000240000@gmail.com>
2019-10-17 18:31:28 +0900

    <> ×

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "chats-space"
 set :repo_url,  'git@github.com:<hiroki000024>/<chats-space>.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
-set :rbenv_ruby, '<2.5.1>'
+set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/ChatsSpace.pem']
 
